8b6d6220e84f1880c6872f8f979017f5776d744b
Skeletonek <skeleton0199@gmail.com>
2022-06-16 18:23:32 +0200

    clear Fedora KDE code & check for superuser

--- Fedora_36_KDE.sh
+++ Fedora_36_KDE.sh
@@ -16,38 +16,56 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 #    Contact: skeleton0199@gmail.com
-echo "Fedora Post-Install Configuration Script"
-echo ""
-echo "Version: 0.1-36-KDE"
-echo "Author: Skeletonek (skeleton0199@gmail.com)"
-echo ""
-echo "This script configures some things in Fedora after fresh install"
-echo "Things that this script change:"
-echo " - Basic DNF configuration"
-echo " - Updating system"
-echo " - Enabling RPM Fusion Free & Non-Free"
-echo " - Enable Flathub repository in Flatpak"
-echo ""
-echo "Use this script only on Fedora KDE Spin"
-echo "This script was made particulary for version 36 but it should work for 35 and 34 as well"
-echo "This script must be run in su mode to complete all functions"
-echo "Otherwise the script will run into some errors"
-echo "If you are ready to start this script type READY uppercase"
-echo "If you type anything diffrent, the script will terminate"
+clear
+cat << EOF
+Fedora Post-Install Configuration Script
+
+Version: 0.2-36-KDE
+Author: Skeletonek (skeleton0199@gmail.com)
+
+This script configures some things in Fedora after fresh install
+Things that this script change:
+ - Basic DNF configuration (SU)
+ - Updating system (SU)
+ - Enabling RPM Fusion Free & Non-Free (SU)
+ - Enable Flathub repository in Flatpak
+
+Use this script only on Fedora KDE Spin
+This script was made particulary for version 36 but it should work for 35 and 34 as well
+This script must be run in su mode to complete all functions
+
+EOF
+if [[ $(id -u) != 0 ]] ; then
+  cat << EOF
+  ! You didn't run this script as super user. Some commands will not work !
+
+EOF
+fi
+
+cat << EOF
+If you are ready to start this script type READY uppercase
+If you type anything diffrent, the script will terminate
+EOF
 read -p ": " ready_string
 if [[ $ready_string == "READY" ]] ; then
 
 # DNF CONFIGURATION
-  printf "# Custom config beneath" >> /etc/dnf/dnf.conf
-  printf "max_parallel_download=10" >> /etc/dnf/dnf.conf
-  printf "fastestmirror=True" >> /etc/dnf/dnf.conf
-  printf "keepcache=True" >> /etc/dnf/dnf.conf
+  if [[ $(id -u) == 0 ]] ; then
+    printf "# Custom config beneath" >> /etc/dnf/dnf.conf
+    printf "max_parallel_download=10" >> /etc/dnf/dnf.conf
+    printf "fastestmirror=True" >> /etc/dnf/dnf.conf
+    printf "keepcache=True" >> /etc/dnf/dnf.conf
+  fi
 
 # SYSTEM UPDATE
-  dnf update -y
+  if [[ $(id -u) == 0 ]] ; then
+    dnf update -y
+  fi
 
 # ADD RPMFUSION FREE & NON-FREE TO DNF
-  sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  if [[ $(id -u) == 0 ]] ; then
+    sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  fi
 
 # ADD FLATHUB REPOSITORY TO FLATPAK
   flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
