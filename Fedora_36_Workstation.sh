dc8c7ef568fc897b7350c6e4aac300d173e84bbe
Skeletonek <skeleton0199@gmail.com>
2022-08-05 22:30:14 +0200

    Updated copyright info

--- Fedora_36_Workstation.sh
+++ Fedora_36_Workstation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#	 Copyright (C) 2021 Skeletonek
+#	 Copyright (C) 2022 Skeletonek
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
